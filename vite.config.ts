d11c751d192eeaae93bc99be882247ca8d5ef201
Twichher <vladickflorin@gmail.com>
2024-11-16 10:35:05 +0300

    5

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": { 
-        target: "http://109.252.65.250:8000",
+        target: "http://localhost:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/"),
       },
