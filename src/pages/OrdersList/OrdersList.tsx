8e83fde32993bad823a28ef44d8cd8ea4725492f
Twichher <vladickflorin@gmail.com>
2024-12-25 07:39:28 +0300

    last version

--- src/pages/OrdersList/OrdersList.tsx
+++ src/pages/OrdersList/OrdersList.tsx
@@ -11,6 +11,23 @@ import { useNavigate } from "react-router-dom";
 
 export const OrdersList = () => {
 
+    const [statusFilter, setStatusFilter] = useState(""); 
+    const [startDateFilter, setStartDateFilter] = useState("");
+    const [byDateFilter, setByDateFilter] = useState("");
+
+    const handleStatusFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setStatusFilter(e.target.value);
+      };
+    
+      const handleStartDateFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setStartDateFilter(e.target.value);
+      };
+    
+      const handleByDateFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setByDateFilter(e.target.value);
+      };
+    
+
     const username = useSelector((state: RootState) => state.cookie.username);
     const [allOrders, SetAllOrders] = useState<getOrderListOfUserType[] | null>(null)
     const dispatch = useDispatch<AppDispatch>();
@@ -26,7 +43,40 @@ export const OrdersList = () => {
             () => {}, 
             null
             );
-            if (allOrders.payload && "data" in allOrders.payload) SetAllOrders(allOrders.payload.data )
+
+            console.log(allOrders.payload)
+            if (allOrders.payload && "data" in allOrders.payload) {
+                let filteredOrders = allOrders.payload.data;
+          
+                if (statusFilter !== "") {
+                  filteredOrders = filteredOrders.filter(
+                    (order) => order.status_order === parseInt(statusFilter)
+                  );
+                }
+          
+
+                if (startDateFilter !== "" && byDateFilter !== "") {
+                  filteredOrders = filteredOrders.filter(
+                    (order) =>
+                      new Date(startDateFilter) <= new Date(order.d_start) &&
+                      new Date(order.d_start) <= new Date(byDateFilter)
+                  );
+                } else if (startDateFilter !== "") {
+
+                  filteredOrders = filteredOrders.filter(
+                    (order) => new Date(startDateFilter) <= new Date(order.d_start)
+                  );
+                } else if (byDateFilter !== "") {
+
+                  filteredOrders = filteredOrders.filter(
+                    (order) => new Date(order.d_start) <= new Date(byDateFilter)
+                  );
+                }
+          
+
+                SetAllOrders(filteredOrders);
+            }
+
 
         } catch {
 
@@ -40,6 +90,16 @@ export const OrdersList = () => {
 
         dispatch(getOrderInfo({id: String(id_order)}));
         navigate(`${ROUTES.ORDER}/${id_order}`)
+
+    }
+
+    const SortOrders = async () => {
+
+        getOrdersInfo()
+
+        // console.log(statusFilter)
+        // console.log(startDateFilter)
+        // console.log(byDateFilter)
 
     }
 
@@ -69,6 +129,41 @@ export const OrdersList = () => {
 
         <div className="AllOrder">
         <h2>Заказы покупателя</h2>
+
+        <div className="filters">
+          <div className="filter">
+            <label htmlFor="statusFilter">Сортировка по статусу:</label>
+            <input
+              id="statusFilter"
+              type="text"
+              placeholder="Введите статус"
+              value={statusFilter}
+              onChange={handleStatusFilterChange}
+            />
+          </div>
+          <div className="filter">
+            <label htmlFor="startDateFilter">Дата создания от:</label>
+            <input
+              id="startDateFilter"
+              type="date"
+              value={startDateFilter}
+              onChange={handleStartDateFilterChange}
+            />
+          </div>
+          <div className="filter">
+            <label htmlFor="formDateFilter">Дата создания до:</label>
+            <input
+              id="formDateFilter"
+              type="date"
+              value={byDateFilter}
+              onChange={handleByDateFilterChange}
+            />
+          </div>
+
+          <button onClick={() => {SortOrders()}}> Сортировать </button>      
+
+        </div>
+
         {allOrders ? (
             allOrders.map((order) => (
             <div key={order.id_order} className="OrderCard">
